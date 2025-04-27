3fd1d3f2766453dbe74e5b53ef5607cce6e3a0f3
peternmuller <pm563838@gmail.com>
2025-04-27 19:00:23 +0200

    templates: props: Update to 27.1
    ref: https://t.me/emhubf3/1204944
    Also add and keep SF duration props from 26.1
    
    Signed-off-by: peternmuller <pm563838@gmail.com>

---
+++ OverlayFS Template/service.sh
@@ -0,0 +1,5 @@
+resetprop --delete debug.sf.high_fps_early_gl_phase_offset_ns
+resetprop --delete debug.sf.high_fps_early_phase_offset_ns
+resetprop --delete debug.sf.high_fps_late_app_phase_offset_ns
+resetprop --delete debug.sf.high_fps_late_sf_phase_offset_ns
+resetprop --delete debug.sf.enable_advanced_sf_phase_offset
